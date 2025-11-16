3c1a6c40a3600b748ba826b7b57eadb2a91a89bd
pranaykawadkar2006 <pranaykawadkar2006@gmail.com>
2025-11-16 14:50:13 +0530

    Update 63_6_DecimalToBinary_kawadkar.c

--- 63_6_DecimalToBinary_kawadkar.c
+++ 63_6_DecimalToBinary_kawadkar.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    int b[32];
+    int i = 0;
+
+    scanf("%d", &n);
+
+    if (n == 0)
+    {
+        printf("0\n");
+        return 0;
+    }
+
+    while (n)
+    {
+        b[i++] = n % 2;
+        n /= 2;
+    }
+
+    while (i--)
+    {
+        printf("%d", b[i]);
+    }
+
+    printf("\n");
+
+    return 0;
+}
 
